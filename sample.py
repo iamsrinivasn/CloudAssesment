b72e364c8c76446664e1db6372a5f97c163ca5e6
Srinivas Niddapu <srini@axiomio.com>
2022-10-13 18:05:50 +0530

    get-subnet-igw

--- sample.py
+++ sample.py
@@ -1,9 +1,30 @@
 import json
 import boto3
-AWS_REGION = "us-west-1"
 
+# Set AWS Region
+AWS_REGION = "us-west-1"
 ec2 = boto3.client('ec2', region_name=AWS_REGION)
-response = ec2.describe_vpcs()
-for val in response['Vpcs']:
-    print(val['VpcId'])
+
+# List all VPCs in the selected Region
+print("")
+print("1. List all VPCs in the selected Region")
+desc_vpcs = ec2.describe_vpcs()
+for vpc in desc_vpcs['Vpcs']:
+    print(vpc['VpcId']+" "+vpc['CidrBlock'])
+
+# List all Subnet in the selected Region
+print("")
+print("2. List all Subnet in the selected Region")
+desc_subs = ec2.describe_subnets()
+for subnet in desc_subs['Subnets']:
+    print(subnet['VpcId']+" "+subnet['SubnetId']+" "+subnet['CidrBlock']+" "+subnet['AvailabilityZone'])
+
+# List all Internet Gateways in the selected Region
+print("")
+print("2. List all IGW in the selected Region")
+desc_igws = ec2.describe_internet_gateways()
+for igw in desc_igws['InternetGateways']:
+    print(igw['Attachments'][0]['VpcId']+" "+igw['InternetGatewayId'])
+
+
 
